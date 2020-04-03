b12c450b99f7425d3ee836990bcaf39a59c44750
xiaorui <258453601@qq.com>
2020-04-04 00:43:58 +0800

    NetWorkStarted..

---
+++ Foundation/Assets/Scripts/Msg/CustomMsg.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+    
+namespace Msg
+{
+    public class CustomMsg : MessageBase
+    {
+        public int msgId;
+
+        public string content;
+
+        public Vector3 pos;
+
+        public byte[] bytes;
+
+        public override void Serialize(NetworkWriter writer)
+        {
+            base.Serialize(writer);
+            writer.Write(msgId);
+            writer.Write(content);
+            writer.Write(pos);
+            writer.WriteBytesAndSize(bytes, bytes.Length);
+        }
+
+        public override void Deserialize(NetworkReader reader)
+        {
+            base.Deserialize(reader);
+            msgId = reader.ReadInt32();
+            content = reader.ReadString();
+            pos = reader.ReadVector3();
+            bytes = reader.ReadBytesAndSize();
+        }
+    }
+}
---
+++ Foundation/Assets/Scripts/NetworkRoot.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkRoot : MonoBehaviour
+{
+
+
+    public void StartServer()
+    {
+        NetworkServer.RegisterHandler(MsgType.Connect, OnClientconnect);
+        NetworkServer.RegisterHandler(MsgType.Error, (netMsg) =>
+        {
+            Debug.Log("onError:" + netMsg.msgType);
+        });
+        bool ret = NetworkServer.Listen(20086);
+        if (ret)
+        {
+            Debug.Log("Server started...");
+        }
+        else
+        {
+            Debug.Log("Server start error...");
+        }
+    }
+
+    public void OnClientconnect(NetworkMessage netMsg)
+    {
+        var connet = netMsg.conn;
+        Debug.Log("Client Connect CLientIP:" + connet.address);
+    }
+    
+
+    public void StartClient()
+    {
+        var client = new NetworkClient();
+        client.RegisterHandler(MsgType.Connect, (netMsg) =>
+        {
+            Debug.Log("connet server.. ");
+        });
+        client.Connect("127.0.0.1", 20086);
+    }
+}
