0babe14e3d37494b84e0d8765e976d155792bb2d
xiaorui <258453601@qq.com>
2020-04-09 00:35:36 +0800

    test outline..

---
+++ Foundation/Assets/TestMono.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class TestMono : MonoBehaviour
+{
+    public Material mat;
+    // Start is called before the first frame update
+    private CommandBuffer cb;
+
+    private Renderer render;
+    public Material defaultMat;
+    private void Awake()
+    {
+        cb = new CommandBuffer();
+        render = this.GetComponent<SkinnedMeshRenderer>();
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnWillRenderObject()
+    {
+        // cb.Clear();
+        // cb.DrawRenderer(render, mat);
+        // Graphics.ExecuteCommandBuffer(cb);
+    }
+
+    private void OnRenderObject()
+    {
+        cb.Clear();
+        cb.DrawRenderer(render, mat);
+        cb.DrawRenderer(render, defaultMat);
+        Graphics.ExecuteCommandBuffer(cb);
+    }
+}
